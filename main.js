aaab594a506837538b41e0ef5f6e3ac6409fa5d1
Shafin Ashraf <shafinashraf074@gmail.com>
2021-05-31 15:20:45 +0600

    Initital commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    // macOS specific behaviour
+    // can be removed because not going to support macOS
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    // windows and linux users expect to close app when all windows are closed
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
