0134ced2a40dad5395692e912ee4a5a3e603f8ed
Shafin Ashraf <shafinashraf074@gmail.com>
2021-07-02 18:19:55 +0600

    Rewrite comment and string highlighting

--- components/main_window/line.tsx
+++ components/main_window/line.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, CSSProperties, useEffect } from "react";
+import React, { createRef, CSSProperties, useEffect, useMemo } from "react";
 import { color } from "./util/color";
 
 export type LineType = { text: string; active: boolean; caretPosition: number };
@@ -36,193 +36,81 @@ export type LinePropType = {
 };
 
 function Line(props: LinePropType) {
-  const lineDivRef = createRef<HTMLPreElement>();
+  const highLightCommentAndString = (htmlString: string): string => {
+    let highlightedString = "";
+    let pointer = 0;
 
-  useEffect(() => {
-    if (props.active && lineDivRef.current !== null) {
-      lineDivRef.current.focus();
-      if (lineDivRef.current.childNodes.length > 0) {
-        // Setting caret properly using props.caretPosition.
-        // Line component can have textnode inside span elem or only textnode.
-        // props.caretPosition is offset from the start of the line, but
-        // caret only be set only in textnode and takes local offset.
-        // So traversing line componensts all child nodes and calculating local caret offset
-        const range = document.createRange();
-        let adjustedCaretPosition = props.caretPosition;
-        for (let child of Array.from(lineDivRef.current.childNodes)) {
-          if (child.nodeName == "SPAN") {
-            let c = child as HTMLElement;
-            if (adjustedCaretPosition > c.innerText.length) {
-              let c = child as HTMLElement;
-              adjustedCaretPosition -= c.innerText.length;
-            } else {
-              if (child.firstChild != null) {
-                range.setStart(child.firstChild, adjustedCaretPosition);
-              }
-              range.collapse(true);
-              const selection = window.getSelection();
-              if (selection !== null) {
-                selection.removeAllRanges();
-                selection.addRange(range);
-              }
-
-              break;
-            }
-          } else {
-            if (
-              child.textContent != null &&
-              adjustedCaretPosition > child.textContent.length
-            ) {
-              if (child.textContent !== null) {
-                adjustedCaretPosition -= child.textContent.length;
-              }
-            } else {
-              range.setStart(child, adjustedCaretPosition);
-              range.collapse(true);
-              const selection = window.getSelection();
-              if (selection !== null) {
-                selection.removeAllRanges();
-                selection.addRange(range);
-              }
-
-              break;
-            }
-          }
-        }
+    if (props.insideCommentBlock.current) {
+      const closingHashPos = htmlString.indexOf("#");
+      if (closingHashPos == -1) {
+        // whole line must be comment
+        return `<span style='color:${color.gray};'>` + htmlString + "</span>";
+      } else {
+        // line contains closing #
+        highlightedString +=
+          `<span style='color:${color.gray};'>` +
+          htmlString.substring(0, closingHashPos + 1) +
+          "</span>";
+        pointer = closingHashPos + 1;
+        props.insideCommentBlock.current = false;
       }
     }
-  });
+    while (pointer < htmlString.length) {
+      if (htmlString.charAt(pointer) === "#") {
+        let closingHashFound = false;
+        // closing comment case was handled previously so this must opening #
+        // for one line or multiline comment
+        for (let i = pointer + 1; i < htmlString.length; i++) {
+          if (htmlString.charAt(i) === "#") {
+            highlightedString +=
+              `<span style='color:${color.gray};'>` +
+              htmlString.substring(pointer, i + 1) +
+              "</span>";
+            pointer = i + 1;
+            closingHashFound = true;
+            break;
+          }
+        }
+        if (!closingHashFound) {
+          // didn't find closing # in loop above
+          // so, must be opening # of a multiline comment
+          props.insideCommentBlock.current = true;
+
+          highlightedString +=
+            `<span style='color:${color.gray};'>` +
+            htmlString.substring(pointer, htmlString.length) +
+            "</span>";
+          pointer = htmlString.length;
+        }
+      } else if (htmlString.charAt(pointer) === '"') {
+        for (let i = pointer + 1; i < htmlString.length; i++) {
+          if (htmlString.charAt(i) === '"') {
+            highlightedString +=
+              `<span style='color:${color.green};'>` +
+              htmlString.substring(pointer, i + 1) +
+              "</span>";
+            pointer = i + 1;
+            break;
+          }
+        }
+        highlightedString += htmlString.charAt(pointer);
+        pointer++;
+      } else {
+        highlightedString += htmlString.charAt(pointer);
+        pointer++;
+      }
+    }
+    return highlightedString;
+  };
 
   const highLight = (htmlString: string): string => {
     if (props.lineIndex === 0) {
       props.insideCommentBlock.current = false;
     }
 
-    // Handling multiline comment block
-    const hashSigns = [];
-    for (let i = 0; i < htmlString.length; i++) {
-      if (htmlString.charAt(i) === "#") {
-        hashSigns.push(i);
-      }
-    }
-    // if hashsign not empty line must have #
-    if (hashSigns.length > 0) {
-      // must be closing # for comment block
-      if (props.insideCommentBlock.current) {
-        const beforeHashSign = htmlString.substring(0, hashSigns[0] + 1);
-        const afterHashSign = htmlString.substring(
-          hashSigns[0] + 1,
-          htmlString.length
-        );
-        console.log("closing: ", beforeHashSign, afterHashSign);
-        htmlString =
-          `<span style='color:grey;'>${beforeHashSign}</span>` + afterHashSign;
-
-        //debugger;
-        props.insideCommentBlock.current = false;
-        // After closing there still could be a comment block opening
-        const remainingHashSigns = [];
-        for (let i = 0; i < afterHashSign.length; i++) {
-          if (afterHashSign.charAt(i) === "#") {
-            remainingHashSigns.push(i);
-          }
-        }
-        if (remainingHashSigns.length > 0) {
-          if (remainingHashSigns.length % 2 !== 0) {
-            const newBeforeHashSign = afterHashSign.substring(
-              0,
-              remainingHashSigns[remainingHashSigns.length - 1]
-            );
-            const newAfterHashSign = afterHashSign.substring(
-              remainingHashSigns[remainingHashSigns.length - 1],
-              afterHashSign.length
-            );
-            console.log("opening: ", newBeforeHashSign, newAfterHashSign);
-
-            htmlString =
-              `<span style='color:${color.gray};'>${beforeHashSign}</span>` +
-              newBeforeHashSign +
-              `<span style='color:${color.gray};'>${newAfterHashSign}</span>`;
-
-            //debugger;
-            props.insideCommentBlock.current = true;
-          } else {
-            htmlString = `<span style='color:${color.gray};'>${beforeHashSign}</span>`;
-            for (let i = 0; i < remainingHashSigns.length; i += 2) {
-              htmlString +=
-                afterHashSign.substring(i, remainingHashSigns[i]) +
-                `<span style='color:${color.gray};'>${afterHashSign.substring(
-                  remainingHashSigns[i],
-                  remainingHashSigns[i + 1] + 1
-                )}</span>`;
-              if (i + 2 < remainingHashSigns.length) {
-                htmlString += afterHashSign.substring(
-                  remainingHashSigns[i + 1] + 1,
-                  remainingHashSigns[i + 2]
-                );
-              } else if (remainingHashSigns[i + 1] < afterHashSign.length) {
-                htmlString += afterHashSign.substring(
-                  remainingHashSigns[i + 1] + 1,
-                  afterHashSign.length
-                );
-              }
-            }
-          }
-        }
-      }
-      // must be opening # for comment block
-      else if (hashSigns.length % 2 !== 0) {
-        const beforeHashSign = htmlString.substring(
-          0,
-          hashSigns[hashSigns.length - 1]
-        );
-        const afterHashSign = htmlString.substring(
-          hashSigns[hashSigns.length - 1],
-          htmlString.length
-        );
-        console.log("opening: ", beforeHashSign, afterHashSign);
-
-        htmlString =
-          beforeHashSign +
-          `<span style='color:${color.gray};'>${afterHashSign}</span>`;
-
-        //debugger;
-        props.insideCommentBlock.current = true;
-      }
-    }
-    // doesn't have # but could be in multiline comment block
-    else {
-      // definitely in multiline comment block
-      // whole line should be in span with grey color
-      if (props.insideCommentBlock.current) {
-        return `<span style='color:${color.gray};'>${htmlString}</span>`;
-      }
-    }
+    htmlString = highLightCommentAndString(htmlString);
 
     return htmlString
-      .replace(/<span.*?#.*?#.*?<\/span>|(#.*?#)/gu, (m, group1) => {
-        if (group1 === undefined) {
-          return m.replace(
-            /(<\/span>.*?)(#.*?#)(.*?<span)/gu,
-            (newM, newGoup1, newGroup2, newGroup3): string => {
-              if (newM) {
-                return `${newGoup1}<span style='color:${color.gray};'>${newGroup2}</span>${newGroup3}`;
-              } else {
-                return newM;
-              }
-            }
-          );
-        } else {
-          return `<span style='color:${color.gray};'>${group1}</span>`;
-        }
-      })
-      .replace(/<span.*?".*?".*?<\/span>|(".*?")/gu, (match, group1) => {
-        if (group1 === undefined) {
-          return match;
-        } else {
-          return `<span style='color:${color.green};'>${group1}</span>`;
-        }
-      })
       .replace(/<span.*?ফাং.*?<\/span>|(ফাং)/gu, (m, group1) => {
         if (group1 === undefined) {
           return m.replace(
@@ -299,6 +187,67 @@ function Line(props: LinePropType) {
     return htmlString;
   };
 
+  const escapedAndHighlightedHtml = useMemo(
+    () => escapeHtml(props.txt),
+    [props]
+  );
+
+  const lineDivRef = createRef<HTMLPreElement>();
+  useEffect(() => {
+    if (props.active && lineDivRef.current !== null) {
+      lineDivRef.current.focus();
+      if (lineDivRef.current.childNodes.length > 0) {
+        // Setting caret properly using props.caretPosition.
+        // Line component can have textnode inside span elem or only textnode.
+        // props.caretPosition is offset from the start of the line, but
+        // caret only be set only in textnode and takes local offset.
+        // So traversing line componensts all child nodes and calculating local caret offset
+        const range = document.createRange();
+        let adjustedCaretPosition = props.caretPosition;
+        for (let child of Array.from(lineDivRef.current.childNodes)) {
+          if (child.nodeName == "SPAN") {
+            let c = child as HTMLElement;
+            if (adjustedCaretPosition > c.innerText.length) {
+              let c = child as HTMLElement;
+              adjustedCaretPosition -= c.innerText.length;
+            } else {
+              if (child.firstChild != null) {
+                range.setStart(child.firstChild, adjustedCaretPosition);
+              }
+              range.collapse(true);
+              const selection = window.getSelection();
+              if (selection !== null) {
+                selection.removeAllRanges();
+                selection.addRange(range);
+              }
+
+              break;
+            }
+          } else {
+            if (
+              child.textContent != null &&
+              adjustedCaretPosition > child.textContent.length
+            ) {
+              if (child.textContent !== null) {
+                adjustedCaretPosition -= child.textContent.length;
+              }
+            } else {
+              range.setStart(child, adjustedCaretPosition);
+              range.collapse(true);
+              const selection = window.getSelection();
+              if (selection !== null) {
+                selection.removeAllRanges();
+                selection.addRange(range);
+              }
+
+              break;
+            }
+          }
+        }
+      }
+    }
+  });
+
   const lineNumber = (props.lineIndex + 1).toString();
   let pads = "";
   for (let i = 0; i < props.padding; i++) {
@@ -310,7 +259,7 @@ function Line(props: LinePropType) {
       <pre
         style={{ minWidth: "10%" }}
         ref={lineDivRef}
-        dangerouslySetInnerHTML={{ __html: escapeHtml(props.txt) }}
+        dangerouslySetInnerHTML={{ __html: escapedAndHighlightedHtml }}
         contentEditable={true}
         onClick={(_) => props.handleOnClick(props.lineIndex)}
         onInput={(e) => {
